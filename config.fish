fd812128ac0a736633ba322c5a86bc3312d48a1f
Kaiyang Lyu <kevinlui598@gmail.com>
2023-06-23 21:15:22 +0800

    add fish config

---
+++ config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias e='emacsclient -c'
+    alias ed='emacs --daemon'
+    alias mg++='g++ -g -Wall -Wextra -std=gnu++2a'
+end
