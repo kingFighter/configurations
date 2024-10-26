8dd614721d266138f1ba9d9611aa680aec309b5a
Kaiyang Lyu <kevin.lyu@crypto.com>
2024-10-27 03:22:01 +0800

    fish: update fish and true color test script

--- config.fish
+++ config.fish
@@ -1,7 +1,10 @@
+# path: ~/.config/fish/config.fish
+# based on `oh my fish`
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias e='emacsclient -c'
     alias ed='emacs --daemon'
-    alias mg++='g++ -g -Wall -Wextra -std=c++20 -pthread'
+    alias mg++='g++ -g -Wall -Wextra -std=c++20'
     fish_add_path -p /home/kaiyang/.local/bin
+    set -gx COLORTERM "truecolor"
 end
