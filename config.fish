7565cb2c07c1f1e8c957ef7021fbefe990f0d44a
Kaiyang Lyu <kevinlui598@gmail.com>
2025-05-10 11:48:45 +0800

    fish: update C++ to 23

--- config.fish
+++ config.fish
@@ -4,7 +4,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias e='emacsclient -c'
     alias ed='emacs --daemon'
-    alias mg++='g++ -g -Wall -Wextra -std=c++20'
+    alias mg++='g++ -g -Wall -Wextra -std=c++23'
     fish_add_path -p /home/kaiyang/.local/bin
     set -gx COLORTERM "truecolor"
 end
